a4e51af2fa6e8ddd489225162758007e15d0e9a4
김민욱 <kmwpcpk@gmail.com>
2025-10-14 17:14:13 +0900

    1001

---
+++ BOJ_java/src/com/kruk/class1/_1001.java
@@ -0,0 +1,22 @@
+package com.kruk.class1;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _1001 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[2];
+		for(int i=0; i<2; i++) arr[i] = Integer.parseInt(st.nextToken());
+		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(String.valueOf(arr[0] - arr[1]));
+		bw.flush();
+		bw.close();
+	}
+}
