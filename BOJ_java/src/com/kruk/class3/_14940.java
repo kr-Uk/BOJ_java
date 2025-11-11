bd2d84870a33a88a22010905e44e96fe71a485d5
김민욱 <kmwpcpk@gmail.com>
2025-11-11 17:56:29 +0900

    1111

--- BOJ_java/src/com/kruk/class3/_14940.java
+++ BOJ_java/src/com/kruk/class3/_14940.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class _14940{
@@ -18,6 +20,8 @@ public class _14940{
 class Solution14940{
 	
 	static int[][] graph;
+	static int[] dx = new int[] {1, -1, 0, 0};
+	static int[] dy = new int[] {0, 0, 1, -1};
 	
     void solve() throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,19 +34,59 @@ class Solution14940{
         
         graph = new int[n][m];
         
+        int start_x = 0;
+        int start_y = 0;
+        
         for(int i=0; i<n; i++) {
         	st = new StringTokenizer(br.readLine());
-        	for(int j=0; j<m; j++) graph[i][j] = Integer.parseInt(st.nextToken());
+        	for(int j=0; j<m; j++) {
+        		graph[i][j] = Integer.parseInt(st.nextToken());
+        		if (graph[i][j] == 2) {
+        			start_x = j;
+        			start_y = i;
+        		}
+        	}
         }
         
-        bfs(n, m);
+        bfs(n, m, start_x, start_y);
+        
+        for(int i=0; i<n; i++) {
+        	for (int j=0; j<m; j++) {
+        		bw.write(String.valueOf(graph[i][j]) + " ");
+        	}
+        	bw.newLine();
+        }
         
         bw.flush();
         bw.close();
     }
 
-	private void bfs(int n, int m) {
+	private void bfs(int n, int m, int x, int y) {
+		Queue<int[]> q = new LinkedList<int[]>();
+		q.add(new int[] {x, y});
+		graph[y][x] = 0;
 		
+		while(!q.isEmpty()) {
+			int[] coor = q.poll();
+			for (int i=0; i<4; i++) {
+				int _x = coor[0] + dx[i];
+				int _y = coor[1] + dy[i];
+				if (_x >= 0 && _x < m && _y >= 0 && _y < n){
+					if ((graph[_y][_x] == 1 || graph[_y][_x] > graph[coor[1]][coor[0]]+1)
+							&& graph[_y][_x] != 0) {
+						q.add(new int[] {_x, _y});
+						graph[_y][_x] = graph[coor[1]][coor[0]] + 1;
+					}
+				}
+			}
+		}
 		
+		for (int i=0; i<4; i++) {
+			int _x = x + dx[i];
+			int _y = y + dy[i];
+			if (_x >= 0 && _x < m && _y >= 0 && _y < n){
+				graph[_y][_x] = 1;
+			}
+		}
 	}
 }
